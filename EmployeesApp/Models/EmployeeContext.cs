6d4c9c96e3e246d847fb1d0fb4a39c4f14598b24
Gah7583 <luismauricio.devpro@gmail.com>
2024-06-27 16:56:23 -0300

    Projeto criado e iniciado.

---
+++ EmployeesApp/Models/EmployeeContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeesApp.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(){ };
+        }
+    }
+}
