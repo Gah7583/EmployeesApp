3db1b96d8a529833c3c4870d1bc054b81c224b38
Gah7583 <luismauricio.devpro@gmail.com>
2024-07-01 16:39:47 -0300

    Fluent Assertions added and Account Number Validation Tests remade with it.

--- EmployeesApp.Tests/Validation/AccountNumberValidationTests.cs
+++ EmployeesApp.Tests/Validation/AccountNumberValidationTests.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeesApp.Validation;
+using FluentAssertions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,22 +16,22 @@ namespace EmployeesApp.Tests.Validation
         public AccountNumberValidationTests() => _validation = new AccountNumberValidation();
 
         [Fact]
-        public void IsValid_ValidAccountNumber_ReturnsTrue() => Assert.True(_validation.IsValid("123-1234567890-17"));
+        public void IsValid_ValidAccountNumber_ReturnsTrue() => _validation.IsValid("123-1234567890-17").Should().BeTrue();
 
         [Theory]
         [InlineData("1234-1234567890-17")]
         [InlineData("12-1234567890-17")]
-        public void IsValid_AccountNumberFirstPartWrong_ReturnsFalse(string accountNumber) => Assert.False(_validation.IsValid(accountNumber));
+        public void IsValid_AccountNumberFirstPartWrong_ReturnsFalse(string accountNumber) => _validation.IsValid(accountNumber).Should().BeFalse();
 
         [Theory]
         [InlineData("123-123456789-17")]
         [InlineData("123-12345678901-17")]
-        public void IsValid_AccountNumberMiddlePartWrong_ReturnsFalse(string accountNumber) => Assert.False(_validation.IsValid(accountNumber));
+        public void IsValid_AccountNumberMiddlePartWrong_ReturnsFalse(string accountNumber) => _validation.IsValid(accountNumber).Should().BeFalse();
 
         [Theory]
         [InlineData("123-1234567890-1")]
         [InlineData("123-1234567890-123")]
-        public void IsValid_AccountNumberLastPartWrong_ReturnsFalse(string accountNumber) => Assert.False(_validation.IsValid(accountNumber));
+        public void IsValid_AccountNumberLastPartWrong_ReturnsFalse(string accountNumber) => _validation.IsValid(accountNumber).Should().BeFalse();
 
         [Theory]
         [InlineData("123-1234567890=12")]
